fafb0bd8f1b252d55f3ef27eac577d42191682fb
coniev <cvconiie@gmail.com>
2024-01-23 15:15:49 -0300

    hola

---
+++ index.php
@@ -0,0 +1,374 @@
+<?php
+
+
+?>
+<!doctype html>
+
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <title>Dr Mario Vega</title>
+
+
+<!-- Favicon -->
+<link href="img/favicon.ico" rel="icon">
+
+<!-- Google Web Fonts -->
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+
+<!-- Libraries Stylesheet -->
+<link href="/ub/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+<link href="/ub/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />  
+
+<!-- Customized Bootstrap Stylesheet -->
+<link href="/assets/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Icon Font Stylesheet -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+<!-- Template Stylesheet -->
+<link href="assets/css/dr.css" rel="stylesheet">
+
+
+  
+<body>
+	
+
+<div class="container-fluid py-2 border-bottom d-none d-lg-block">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-6 text-center text-lg-start mb-2 mb-lg-0">
+                    <div class="d-inline-flex align-items-center">
+                        <a class="text-decoration-none text-body pe-3" href=""><i class="far fa-clock"></i>
+                        Lunes - Martes 9am hasta 19pm</a>
+                        <span class="text-body">|</span>
+                        <a class="text-decoration-none text-body px-3" href=""><i class="bi bi-envelope me-2"></i>mariovega46@gmail.com</a>
+                    </div>
+                </div>
+                <div class="col-md-6 text-center text-lg-end">
+                    <div class="d-inline-flex align-items-center">
+                        <a class="text-body px-2" href="">
+                            <i class="fab fa-facebook-f"></i>
+                        </a>
+                        <a class="text-body px-2" href="">
+                            <i class="fab fa-twitter"></i>
+                        </a>
+                        <a class="text-body px-2" href="">
+                            <i class="fab fa-linkedin-in"></i>
+                        </a>
+                        <a class="text-body px-2" href="">
+                            <i class="fab fa-instagram"></i>
+                        </a>
+                        <a class="text-body ps-2" href="">
+                            <i class="fab fa-youtube"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Topbar End -->
+
+
+    <!-- Navbar Start -->
+    <div class="container-fluid sticky-top bg-white">
+        <div class="container-fluid">
+            <nav class="navbar bor cero navbar-expand-lg bg-white navbar-light py-3 py-lg-0">
+                <a href="http://localhost/portafolio/misitio/drmariovega/" class="navbar-brand">
+                    <div class=""><img class="iconooo img-fluid" src="assets/img/logo.png" alt="">
+                   </div>
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <div class="navbar-nav ms-auto py-0">
+                        <a href="http://localhost/portafolio/misitio/drmariovega/" class="nav-item nav-link active">Home</a>
+                        <a href="http://localhost/portafolio/misitio/sobre-nosotros-mario-vega/" class="nav-item nav-link">Sobre Nosotros</a>
+                        <a href="http://localhost/portafolio/misitio/servicios-mario-vega/" class="nav-item nav-link">Servicios</a>
+                        <a href="http://localhost/portafolio/misitio/mario-vega-noticias/" class="nav-item nav-link">Noticias</a>
+                        <a href="price.html" class="nav-item nav-link">Contacto</a>
+                        
+                            
+                        
+                       
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </div>
+    <!-- Navbar End -->
+
+
+    <!-- Hero Start -->
+    <div class="container-fluid hero-header" >
+    
+    
+        <div class="container py-5">
+            <div class="row justify-content-start">
+                <div class="col-lg-8 text-center text-lg-start">
+                    
+                    <h1 class="display-1 text-white mb-md-4">Un estilo de vida saludable comienza con una consulta médica</h1>
+                    <div class="pt-2">
+                        
+                        <a href="" class="btn btn-outline-light rounded-pill py-md-3 px-md-5 mx-2 boton-borde-mario">Contáctanos</a>
+                    </div>
+                </div>
+           
+        </div>
+    </div>
+    </div>
+    <!-- Hero End -->
+
+
+
+
+    <!-- SERVICIOS-->
+    <h2 class="centro-servicioss negrita">Nuestros Servicios</h2>
+<div class="container  valores">
+<div class="row">
+  
+
+<div class="col-md-4 did">
+  <div class="card fondo-serviciosmariovega ajaa" style="width: 18rem;">
+
+    <img class="card-img-top icono1-mariovega valores-posicion" src="assets/img/icono1.png">
+  <div class="card-body">
+    <h2 class="card-title fondo-serviciosmariovega sub tituloe negrita mo">Consulta Medica</h2>
+	<!-- BOTON MODAL-->
+  
+
+  </div>
+</div>
+</div>
+
+<div class="col-md-4 did">
+  <div class="card fondo-serviciosmariovega ajaa" style="width: 18rem;">
+
+    <img class="card-img-top icono1-mariovega valores-posicion" src="assets/img/icono2.png">
+  <div class="card-body">
+    <h2 class="card-title fondo-serviciosmariovega sub tituloe negrita mo">Evaluación Cardiaca</h2>
+	<!-- BOTON MODAL-->
+	
+
+  </div>
+</div>
+</div>
+<div class="col-md-4 did">
+  <div class="card fondo-serviciosmariovega ajaa" style="width: 18rem;">
+  
+
+    <img class="card-img-top icono1-mariovega valores-posicion" src="assets/img/icono3.png">
+  <div class="card-body">
+    <h2 class="card-title fondo-serviciosmariovega sub tituloe negrita mo">Diagnostico y Tratamiento</h2>
+	<!-- BOTON MODAL-->
+
+
+  </div>
+</div>
+</div>
+</div>
+</div>
+
+<div class="boton-noticiasposicion">
+<a class="btn btn-outline-light botone-mario" href="http://localhost/portafolio/misitio/servicios-mario-vega/">Ver más</a>
+
+</div>
+
+
+
+
+
+
+
+
+    <!-- SOBRE MI -->
+    <div class="container-fluid py-5 margen-sobre">
+        <div class="container">
+            <div class="row gx-5">
+                <div class="col-lg-5 mb-5 mb-lg-0" style="min-height: 500px;">
+                    <div class="position-relative h-100">
+                    
+    <img style="object-fit: cover;" class="position-absolute w-100 h-100 rounded" src="assets/img/img1.jpg"/>
+
+                       
+                    </div>
+                </div>
+                <div class="col-lg-7">
+                    <div class="mb-4">
+                        
+                        <h2 class="display-4 margen-a">Atención cardiológica de calidad</h2>
+                    </div>
+                    <p class="texto-inicio">Si buscas un cardiólogo  profesional y empático que se preocupe por tu bienestar, 
+                        has llegado al lugar indicado.  Mi objetivo es brindar atención médica de calidad, basada en la confianza
+                         y el compromiso con mis pacientes. Juntos trabajaremos para garantizar la mejor atención a tu salud  cardiovascular,
+                          con la seguridad y la tranquilidad que necesitas.</p>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- FIN SOBRE MI-->
+    
+
+  <!-- Blog Start -->
+    <div class="container-fluid py-5">
+    <div class="text-center mx-auto mb-5" style="max-width: 500px;">
+                <h5 class="d-inline-block text-primary text-uppercase border-bottom border-5">Blog Post</h5>
+                <h2 class="display-4">Noticias de la Semana</h2>
+            </div>
+        <div class="container">
+            
+            <div class="row g-5">
+                <div class="col-xl-4 col-lg-6 did">
+                    <div class="bg-light rounded overflow-hidden">
+                        <img class="img-fluid w-100" src="img/blog-1.jpg" alt="">
+                        <div class="p-4">
+                            <a class="h3 d-block mb-3" href="">Fin de la pandemia</a>
+                            <p class="m-0">La emergencia sanitaria por la pandemia de covid-19 ha terminado, anunció la Organización Mundial de la Salud (OMS)...</p>
+                        </div>
+                        <div class="d-flex justify-content-between border-top p-4">
+                            <div class="d-flex align-items-center">
+                            
+    <img width="25" height="25" class="rounded-circle me-2" src="assets/img/user1.jpg" />
+
+                                
+                                <small>John Doe</small>
+                            </div>
+                            <div class="d-flex align-items-center">
+                                <small class="ms-3"><i class="far fa-eye text-primary me-1"></i>12345</small>
+                                <small class="ms-3"><i class="far fa-comment text-primary me-1"></i>123</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-6 did">
+                    <div class="bg-light rounded overflow-hidden">
+                        <img class="img-fluid w-100" src="img/blog-2.jpg" alt="">
+                        <div class="p-4">
+                            <a class="h3 d-block mb-3" href="">Trasplante del corazón de un cerdo modificado</a>
+                            <p class="m-0">Un equipo de cirujanos trasplantó un corazón de un cerdo modificado genéticamente a un humano fallecido...</p>
+                        </div>
+                        <div class="d-flex justify-content-between border-top p-4">
+                            <div class="d-flex align-items-center">
+                            
+                                <img width="25" height="25" class="rounded-circle me-2" src="assets/img/user2.png" />
+
+                                <small>John Doe</small>
+                            </div>
+                            <div class="d-flex align-items-center">
+                                <small class="ms-3"><i class="far fa-eye text-primary me-1"></i>12345</small>
+                                <small class="ms-3"><i class="far fa-comment text-primary me-1"></i>123</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-6 did">
+                    <div class="bg-light rounded overflow-hidden">
+                        <img class="img-fluid w-100" src="img/blog-3.jpg" alt="">
+                        <div class="p-4">
+                        <a class="h3 titulo-noticia d-block mb-3" href="">Vacuna de la Influenza></a>
+                            <p class="m-0">Cuando se anunció el caso de un estadounidense que había entrado en remisión del VIH en julio de 2022....</p>
+                        </div>
+                        <div class="d-flex justify-content-between border-top p-4">
+                            <div class="d-flex align-items-center">
+                           
+                                <img width="25" height="25" class="rounded-circle me-2" src="assets/img/user3.png" />
+
+                                <small>John Doe</small>
+                            </div>
+                            <div class="d-flex align-items-center">
+                                <small class="ms-3"><i class="far fa-eye text-primary me-1"></i>12345</small>
+                                <small class="ms-3"><i class="far fa-comment text-primary me-1"></i>123</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Blog End -->
+    
+    <article class="mi">
+    
+    <img  class="img-fluid hero-lo" src="assets/img/mockup.png" />
+
+
+    </article>
+
+    <!-- Footer Start -->
+    <div class="container-fluid fondos text-light">
+        <div class="container py-5">
+            <div class="row g-5 texto">
+                <div class="col-lg-4 col-md-8">
+                    <h4 class="d-inline-block text-primary text-uppercase border-bottom border-5 border-secondary mb-4">Mantente en Contacto</h4>
+                    <p class="mb-4">Tu corazón, nuestra pasión.</p>
+                    <p class="mb-2"><i class="fa fa-map-marker-alt text-primary me-3"></i>Av.  Bernardo O'Higgins 1614, Santiago</p>
+                    <p class="mb-2"><i class="fa fa-envelope text-primary me-3"></i>mariovega46@gmail.com</p>
+                    <p class="mb-0 cursiva">Lunes - Martes 9am hasta 19pm</p>
+                </div>
+                <div class="col-lg-4 col-md-8">
+                    <h4 class="d-inline-block text-primary text-uppercase border-bottom border-5 border-secondary mb-4"><?php echo esc_html( get_field('titulo1 noticias') ); ?></h4>
+                    <div class="d-flex flex-column justify-content-start">
+                        <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Noticias de la Semana</a>
+                        <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Sobre mí</a>
+                        <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Servicios</a>
+                        <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Vacuna de la Influenza</a>
+                        <a class="text-light" href="#"><i class="fa fa-angle-right me-2"></i>Contacto</a>
+                    </div>
+                </div>
+                
+                <div class="col-lg-4 col-md-8">
+                    <h4 class="d-inline-block text-primary text-uppercase border-bottom border-5 border-secondary mb-4">Newsletter</h4>
+                    <form action="">
+                        <div class="input-group">
+                            <input type="text" class="form-control p-3 border-0" placeholder="Ingresa tu Email">
+                            <button class="btn btn-primary">Suscribete</button>
+                        </div>
+                    </form>
+                    <h6 class="text-primary text-uppercase mt-4 mb-3">Follow Us</h6>
+                    <div class="d-flex">
+                        <a class="btn btn-lg btn-primary btn-lg-square rounded-circle me-2" href="#"><i class="fab fa-twitter"></i></a>
+                        <a class="btn btn-lg btn-primary btn-lg-square rounded-circle me-2" href="#"><i class="fab fa-facebook-f"></i></a>
+                        <a class="btn btn-lg btn-primary btn-lg-square rounded-circle me-2" href="#"><i class="fab fa-linkedin-in"></i></a>
+                        <a class="btn btn-lg btn-primary btn-lg-square rounded-circle" href="#"><i class="fab fa-instagram"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid fondos text-light border-top border-secondary py-4">
+        <div class="container">
+            
+                <div class=" text-center text-md-start">
+                    <p class="mb-md-0">Derechos reservados, Dr. Mario Vega 2023 ©</p>
+                </div>
+                
+           
+        </div>
+    </div>
+    <!-- Footer End -->
+
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-lg lal btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+
+
+   
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/lib/easing/easing.min.js"></script>
+    <script src="assets/lib/waypoints/waypoints.min.js"></script>
+    <script src="assets/lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="assets/lib/tempusdominus/js/moment.min.js"></script>
+    <script src="assets/lib/tempusdominus/js/moment-timezone.min.js"></script>
+    <script src="assets/lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+
+    
+    <script src="assets/js/main.js"></script>
+
+</body>
+</html>
